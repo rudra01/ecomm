ee82c6e258c4b1e2dfc98040ab354bf21f222a95
rudra01 <vikasgupta282@gmail.com>
2022-02-18 15:49:33 +0530

    upadted-logoandappbar

--- lib/main.dart
+++ lib/main.dart
@@ -3,15 +3,24 @@ import 'package:webview_flutter/webview_flutter.dart';
 
 
 void main() {
-  runApp(MaterialApp(
+  runApp(
+    MaterialApp(
     debugShowCheckedModeBanner: false,
     home: Scaffold(
-      appBar: AppBar(
+      appBar: PreferredSize(
+        preferredSize: Size.fromHeight(0),
+        child: AppBar(
+          centerTitle: true,
+          title: Text(
+            "",
+          )
+        )
           // title: Text("First App")
       ),
+      // extendBodyBehindAppBar: true,
       body: WebView(
         initialUrl: "https://www.ardasinterior.com/",
-        javascriptMode: JavascriptMode.unrestricted,
+        javascriptMode: JavascriptMode.unrestricted, 
       ),
     ),
     )
